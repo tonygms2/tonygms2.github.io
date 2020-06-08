56386e0e63885a37f3fb22ff19bfc16984ef1ac0
Nicholas Tony Gomes <tonygms2@gmail.com>
2020-06-08 20:47:34 +0600

    added DOwnload Button

--- app_Slide.js
+++ app_Slide.js
@@ -1,3 +1,8 @@
 const doSomething = ()=>{
   alert("it WOrks")
 }
+
+const Download = ()=>{
+  var url = "https://github.com/tonygms2/Corona-Update/raw/master/CoronaInfoUpdate/app/release/Corona%20Info%20Update%201.5.apk";
+   location.href = url;
+}
